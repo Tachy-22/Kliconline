6ee77fcb0ec711bb21ac06d96e29fa129b53120d
tachy-22 <entekumejeffrey@gmail.com>
2024-11-23 12:22:47 +0100

    vercel fix

--- src/components/layouts/Events.tsx
+++ src/components/layouts/Events.tsx
@@ -7,11 +7,11 @@ import EventsList from "../ui/EventsList";
 const Events = () => {
   return (
     <div className="flex flex-col  py-[2rem] bg-[#F5F2F0]">
-      <div className="h-full  px-6 md:px-20 max-w-7xl mx-auto flex flex-col gap-6">
+      <div className="h-full  px-6 lg::px-20 max-w-7xl mx-auto flex flex-col gap-6">
         <h2 className="text-xl   text-gray-800/50 uppercase max-w-[30rem] ">
           Latest Event{" "}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-[5rem]">
+        <div className="grid grid-cols-1 lg::grid-cols-2 gap-[5rem]">
           <EventDetails />
           <EventRegisterationForm />
         </div>
