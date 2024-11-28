3bece987fc995f04e8c870b0fc859efb7f789a31
tachy-22 <entekumejeffrey@gmail.com>
2024-11-28 22:50:15 +0100

    Event

--- src/app/Events/page.tsx
+++ src/app/Events/page.tsx
@@ -2,10 +2,16 @@ import { fetchCollection } from "@/actions/fettchCollection";
 import Events from "@/components/layouts/Events";
 import React from "react";
 
+
+
 const Page = async () => {
   const events = await fetchCollection("events");
+  
+  const validEvents = "items" in events 
+    ? (events.items as EventT[]).filter(event => event && event.date)
+    : [];
 
-  return <Events events={"items" in events ? (events.items as EventT[]) : []} />;
+  return <Events events={validEvents} />;
 };
 
 export default Page;
