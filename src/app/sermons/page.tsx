5bc3cefb2b4e25f0130ebdb746fed910cfa2def9
tachy-22 <entekumejeffrey@gmail.com>
2024-11-28 23:02:56 +0100

    404

--- src/app/sermons/page.tsx
+++ src/app/sermons/page.tsx
@@ -1,16 +1,17 @@
-import { fetchCollection } from "@/actions/fettchCollection";
-import Sermons from "@/components/layouts/Sermons";
+//import { fetchCollection } from "@/actions/fettchCollection";
+//import Sermons from "@/components/layouts/Sermons";
 import React from "react";
 
 const page = async () => {
-  const sermons = await fetchCollection<SermonT>("sermons");
+  // const sermons = await fetchCollection<SermonT>("sermons");
 
-  console.log({ sermons });
+  // console.log({ sermons });
 
-  if ("items" in sermons) {
-    return <Sermons sermons={sermons.items} />;
-  }
-  return <div>Error </div>;
+  // if ("items" in sermons) {
+  //   return <Sermons sermons={sermons.items} />;
+  // }
+  // return <div>Error </div>;
+  return <div>Sermons</div>
 };
 
 export default page;
