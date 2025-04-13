50096a7c55ddc3468dbb4bfb0e41063b21c43cb0
Tachy-22 <entekumejeffrey@gmail.com>
2025-04-13 16:11:51 +0100

    vercel fix

--- src/components/components/layouts/Contact.tsx
+++ src/components/components/layouts/Contact.tsx
@@ -2,13 +2,13 @@
 
 import React from "react";
 import ContactHero from "../../ui/ContactHero";
-import ContactForm from "../forms/ContactForm";
+//import ContactForm from "../forms/ContactForm";
 
 const Contact = () => {
   return (
     <div className="flex flex-col">
       <ContactHero />
-      <ContactForm />
+      {/* <ContactForm /> */}
     </div>
   );
 };
